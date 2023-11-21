a90cbab43aed69889355b3cfa14c64d7bad52776
Daiana <daiguerr@hotmail.com>
2023-11-21 19:58:17 -0300

    add-confetti

---
+++ main.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const jsConfetti = new JSConfetti();
+  jsConfetti.addConfetti({
+    emojis: ["☕️"],
+    emojiSize: 100,
+    confettiNumber: 50,
+  });
+});
